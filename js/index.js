6179cfb04a3ec779c76333742d80db018e41da8c
kuria  githinji <kurialancelot@gmail.com>
2023-05-31 21:34:21 +0300

    madaraka prep

--- js/index.js
+++ js/index.js
@@ -27,3 +27,19 @@ nameButton.addEventListener("click", async function () {
 
   nameInput.value = "";
 });
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
